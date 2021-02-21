6773c737f70234695e5e01839dd85bc1de176ae0
Shaginyan Artur <arturexp@gmail.com>
2021-02-21 23:00:47 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+have = input("Ваша валюта: ")
+currency_need = input("Валюта которая нужна: ")
+amount = float(input("Количество: "))
+rates = {}
+usd = requests.get("http://www.floatrates.com/daily/usd.json")
+usd = json.loads(usd.text)
+rates['usd'] = usd
+eur = requests.get("http://www.floatrates.com/daily/eur.json")
+eur = json.loads(eur.text)
+rates['eur'] = eur
+
+ex_rate = ["usd", "eur"]
+while True:
+    print('Checking the cache…')
+    if currency_need in ex_rate:
+        print("Oh! It is in the cache!")
+        received = round(float(amount) * rates[currency_need][have]['inverseRate'], 2)
+        print(f"You received {received} {currency_need}.")
+    else:
+        print("Sorry, but it is not in the cache!")
+        ex_rate.append(currency_need)
+        r = requests.get("http://www.floatrates.com/daily/{}.json".format(currency_need))
+        r = json.loads(r.text)
+        rates[currency_need] = r
+        received = round(float(amount) * r[have]['inverseRate'], 2)
+        print(f"You received {received} {currency_need}.")
+    currency_need = input("Валюта которая нужна: ")
+    if currency_need == "":
+        break
+    amount = input("Количество: ")
+    if amount == "":
+        break
+
+
+
+
